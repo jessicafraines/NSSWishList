9609fddc6958d1694e8ec92cb68e9f7d9f16a795
thatknitter <kblewis.flt@gmail.com>
2014-12-10 13:08:18 -0600

    debugging

--- js/controllers/wish.js
+++ js/controllers/wish.js
@@ -1,28 +1,26 @@
-;(function(){
-  'use strict';
-  .controller ('wishController', function($http, $routeParams){
-    var vm = this;
-    var id = $routeParams.id;
-    var url = 'https://nss-group-wishlist.firebaseio.com' + id + '.json';
-    $http.get(url)
-    .success(function(data){
-      vm.newWish = data;
-    })
-    .error(function(err){
-      alert(err);
-    });
-    
-    vm.addItem = function(){
-      $http.post('https://nss-group-wishlist.firebaseio.com/wishlist.json', item)
-      .success(function(data){
-        cb(data);
-      })
-      .error(function(err){
+;( function() {
+    'use strict';
+    angular.module('todoApp', ['ngRoute'])
+    .controller('wishController', function($http, $routeParams) {
+      var vm = this;
+      var id = $routeParams.id;
+      var url = 'https://nss-group-wishlist.firebaseio.com' + id + '.json';
+      $http.get(url)
+      .success(function(data) {
+        vm.newWish = data;
+      }).error(function(err) {
         alert(err);
       });
-      vm.wish.push(vm.newWish);
-      vm.newWish = null;
-    };
-  });
-  
-}());
+
+      vm.addItem = function() {
+        $http.post('https://nss-group-wishlist.firebaseio.com/wishlist.json', item)
+        .success(function(data) {
+          cb(data);
+        }).error(function(err) {
+          alert(err);
+        });
+        vm.wish.push(vm.newWish);
+        vm.newWish = null;
+      };
+    });
+  }());
