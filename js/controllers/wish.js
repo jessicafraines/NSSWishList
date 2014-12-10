452f7600c61991f48fc9874780789372ad670683
thatknitter <kblewis.flt@gmail.com>
2014-12-10 11:49:01 -0600

    started wish controller

--- js/controllers/wish.js
+++ js/controllers/wish.js
@@ -1,5 +1,22 @@
 ;(function(){
   'use strict';
-  
+  .controller ('wishController', function($http, $routeParams){
+    var vm = this;
+    var id = $routeParams.id;
+    var url = 'https://nss-group-wishlist.firebaseio.com' + id + '.json';
+    $http.get(url)
+    .success(function(data){
+      vm.newWish = data;
+    })
+    .error(function(err){
+      alert(err);
+    });
+    
+    vm.addNewWish = function(){
+      
+      vm.wish.push(vm.newWish);
+      vm.newWish = null;
+    };
+  });
   
 }());
